42db450efeaf8b4a02e8ae1ea4f1b8d9cd911117
Andrew Rodrigues <psycho.bunny@hotmail.com>
2019-07-16 01:24:25 -0400

    polish

--- HUD.gd
+++ HUD.gd
@@ -11,7 +11,7 @@ func show_game_over():
 	show_message('Game Over')
 	yield($MessageTimer, 'timeout') # using the created timer (2s)
 	
-	$MessageLabel.text = 'Try again...\nDodge!'
+	$MessageLabel.text = 'Try again!'
 	$MessageLabel.show()
 	yield(get_tree().create_timer(1), 'timeout') # other way using the scene's timer (1s)
 	
