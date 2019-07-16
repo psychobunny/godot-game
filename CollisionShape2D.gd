b10653efa01b8e9440710088b3184eac9296b75b
Andrew Rodrigues <psycho.bunny@hotmail.com>
2019-07-16 00:00:32 -0400

    player moving around

---
+++ CollisionShape2D.gd
@@ -0,0 +1,8 @@
+extends CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
